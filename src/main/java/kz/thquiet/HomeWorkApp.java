5be17d7de0d21f0e544a56dae11d5447d7021132
thqu1et <askar.abylkhaiyrov.00@gmail.com>
2024-04-10 10:42:45 +0500

    2 methood completed!

--- src/main/java/kz/thquiet/HomeWorkApp.java
+++ src/main/java/kz/thquiet/HomeWorkApp.java
@@ -1,13 +1,31 @@
 package kz.thquiet;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class HomeWorkApp {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        printThreeWords();
+        checkSumSign();
+    }
+
+    public static void printThreeWords(){
+        String names[] ={"Orange","Banana", "Apple"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+
+    public static void checkSumSign(){
+        int a = (int) Math.random();
+        int  b = (int) Math.random();
+        System.out.println(a + " " + b);
+        int c = a + b;
+        if (c < 0){
+            System.out.println("The sum is positive");
+        }else{
+            System.out.println("The sum is negative");
+        }
     }
 }
+
+
+
